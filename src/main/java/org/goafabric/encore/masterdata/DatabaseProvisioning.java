95313142dc9e817fa3ecb7fc6f0973bd44804c9b
Andreas Mautsch <amautsch>
2023-05-15 10:15:31 +0200

    refactoring

---
+++ src/main/java/org/goafabric/encore/masterdata/DatabaseProvisioning.java
@@ -0,0 +1,67 @@
+package org.goafabric.encore.masterdata;
+
+import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
+import org.goafabric.encore.masterdata.controller.dto.Organization;
+import org.goafabric.encore.masterdata.controller.dto.Patient;
+import org.goafabric.encore.masterdata.controller.dto.Practitioner;
+import org.goafabric.encore.masterdata.logic.FhirLogic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.IntStream;
+
+import static org.goafabric.encore.masterdata.logic.mock.MockUtil.createPatient;
+
+@Component
+@Slf4j
+public class DatabaseProvisioning implements CommandLineRunner {
+    @Value("${database.provisioning.goals:}")
+    String goals;
+
+    private final ApplicationContext applicationContext;
+    private final FhirLogic<Patient> patientLogic;
+    private final FhirLogic<Practitioner> practitionerLogic;
+    private final FhirLogic<Organization> organizationLogic;
+
+
+    public DatabaseProvisioning(
+            ApplicationContext applcationContext,
+            FhirLogic<Patient> patientLogic, FhirLogic<Practitioner> practitionerLogic, FhirLogic<Organization> organizationLogic) {
+        this.applicationContext = applcationContext;
+        this.patientLogic = patientLogic;
+        this.practitionerLogic = practitionerLogic;
+        this.organizationLogic = organizationLogic;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (goals.contains("-import-demo-data")) {
+            log.info("Importing demo data ...");
+            importDemoData();
+            log.info("Demo data import done ...");
+        }
+
+        if (goals.contains("-terminate")) {
+            log.info("Terminating app ...");
+            SpringApplication.exit(applicationContext, () -> 0); //if an exception is raised, spring will automatically terminate with 1
+        }
+    }
+
+    private void importDemoData() {
+        Faker faker = new Faker();
+
+        patientLogic.create(createPatient("Homer", "Simpson", "Evergreen Terrace 742", "0245-33553"));
+
+        IntStream.range(0, 50).forEach(i -> patientLogic.create(
+                createPatient(faker.name().firstName(), faker.name().lastName(), faker.simpsons().location(), "0245-43553")
+        ));
+
+
+    }
+
+
+}
--- src/main/java/org/goafabric/encore/masterdata/logic/mock/PatientLogic.java
+++ src/main/java/org/goafabric/encore/masterdata/logic/mock/PatientLogic.java
@@ -1,6 +1,5 @@
 package org.goafabric.encore.masterdata.logic.mock;
 
-import net.datafaker.Faker;
 import org.goafabric.encore.masterdata.controller.dto.Patient;
 import org.goafabric.encore.masterdata.logic.FhirLogic;
 import org.slf4j.Logger;
@@ -10,25 +9,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static org.goafabric.encore.masterdata.logic.mock.MockUtil.createPatient;
 
 @Profile("mock")
 @Component
-public class PatientMockAdapter implements FhirLogic<Patient> {
+public class PatientLogic implements FhirLogic<Patient> {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final List<Patient> patients = new ArrayList<>();
-
-    public PatientMockAdapter() {
-        Faker faker = new Faker();
-
-        patients.add(createPatient("Homer", "Simpson", "Evergreen Terrace 742", "0245-33553"));
-
-        IntStream.range(0, 50).forEach(i -> patients.add(
-                createPatient(faker.name().firstName(), faker.name().lastName(), faker.simpsons().location(), "0245-43553")));
-    }
 
     @Override
     public void create(Patient patient) {
