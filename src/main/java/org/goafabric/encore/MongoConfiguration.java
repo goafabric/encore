dc78eba65f8d49ae99f0d5f42b8560695922670a
Andreas Mautsch <amautsch>
2023-05-19 22:04:57 +0200

    jpa changes

--- src/main/java/org/goafabric/encore/MongoConfiguration.java
+++ src/main/java/org/goafabric/encore/MongoConfiguration.java
@@ -2,12 +2,13 @@ package org.goafabric.encore;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("mongodb")
-@EnableAutoConfiguration(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MongoConfiguration {
 }
