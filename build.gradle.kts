07bd07074b7f0ef51bec6551190ea494d5d2011c
Andreas Mautsch <amautsch>
2023-05-19 10:03:55 +0200

    vaadin plugin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 	id("org.graalvm.buildtools.native") version "0.9.20"
 	id("com.google.cloud.tools.jib") version "3.3.1"
 
-	id("com.vaadin") version "24.0.2"
+	id("com.vaadin") version "24.0.5"
 }
 
 jacoco { toolVersion = "0.8.9" }
