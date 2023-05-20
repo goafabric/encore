d9cae1e8cff9f083b0e96fed5cfc88c4631ff5fa
Andreas Mautsch <amautsch>
2023-05-20 09:22:06 +0200

    jpa changes

--- src/test/java/org/goafabric/encore/catalogs/logic/DiagnosisCatalogLogicIT.java
+++ src/test/java/org/goafabric/encore/catalogs/logic/DiagnosisCatalogLogicIT.java
@@ -1,6 +1,7 @@
 package org.goafabric.encore.catalogs.logic;
 
-import org.goafabric.encore.catalogs.logic.mock.DiagnosisCatalogLogic;
+import org.goafabric.encore.catalogs.dto.Diagnosis;
+import org.goafabric.encore.masterdata.logic.CrudLogic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class DiagnosisCatalogLogicIT {
     @Autowired
-    private DiagnosisCatalogLogic diagnosisCatalogLogic;
-
+    private CrudLogic<Diagnosis> diagnosisCatalogLogic;
 
     @Test
     void search() {
