980cf01d5240d694cf99c20205d22a3fd8f534af
Andreas Mautsch <amautsch>
2023-06-03 18:55:14 +0200

    performance

--- src/main/java/org/goafabric/encore/ui/GridView.java
+++ src/main/java/org/goafabric/encore/ui/GridView.java
@@ -1,6 +1,7 @@
 package org.goafabric.encore.ui;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -44,7 +45,9 @@ public abstract class GridView<T> extends VerticalLayout {
     protected abstract void addColumns(Grid<T> grid);
 
     protected void updateList() {
+        long start = System.currentTimeMillis();
         grid.setItems(logic.search(filterText.getValue()));
+        Notification.show("Search took " + (System.currentTimeMillis() -start) + " ms");
     }
 
     protected CrudLogic<T> getLogic() {
