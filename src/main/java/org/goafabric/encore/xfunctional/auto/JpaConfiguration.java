17538815509c812ef43181a5be234c7570ca14d4
Andreas Mautsch <amautsch>
2023-06-05 12:15:58 +0200

    changes

--- src/main/java/org/goafabric/encore/xfunctional/auto/JpaConfiguration.java
+++ src/main/java/org/goafabric/encore/xfunctional/auto/JpaConfiguration.java
@@ -1,11 +1,12 @@
 package org.goafabric.encore.xfunctional.auto;
 
+import org.springframework.boot.actuate.autoconfigure.data.mongo.MongoHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("jpa")
-@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration.class, MongoHealthContributorAutoConfiguration.class})
 public class JpaConfiguration {
 }
